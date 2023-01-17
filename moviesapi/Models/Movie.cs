6bd578dc3b54c537417bf866f93a6f0a0b8e0a8a
Igor Carlos <igor.lima58@gmail.com>
2023-01-17 02:22:52 -0300

    validation added

--- moviesapi/Models/Movie.cs
+++ moviesapi/Models/Movie.cs
@@ -1,11 +1,20 @@
-﻿namespace moviesapi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace moviesapi.Models
 {
     public class Movie
     {
+        [Required(ErrorMessage = "Title is missing.")]
+        [MaxLength(100, ErrorMessage ="Title is too long.")]
         public string Title { get; set; }
+        [Required(ErrorMessage ="Description missing.")]
         public string Description { get; set; }
+        [Required(ErrorMessage ="Director is missing.")]
         public string Director { get; set; }
+        [Required(ErrorMessage = "Year is missing.")]
         public int Year { get; set; }
+        [Required(ErrorMessage = "Duration is missing.")]
+        [Range(15, 500, ErrorMessage ="Should be between 15 to 500 minutes.")]
         public int Minute { get; set; }
 
 
