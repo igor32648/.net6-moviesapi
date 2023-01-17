910216d39a274fa582f7a0130d705b49f0d658fe
Igor Carlos <igor.lima58@gmail.com>
2023-01-17 01:57:32 -0300

    first commit

---
+++ moviesapi/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using moviesapi.Models;
+
+namespace moviesapi.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+
+        [HttpPost]
+        public void AddMovie([FromBody]Movie movie)
+        {
+            movies.Add(movie);
+            Console.WriteLine(movie.Title); 
+            Console.WriteLine(movie.Description);
+            Console.WriteLine(movie.Director);
+        }
+    }
+}
---
+++ moviesapi/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿namespace moviesapi.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Director { get; set; }
+        public int Year { get; set; }
+        public int Minute { get; set; }
+
+
+    }
+}
